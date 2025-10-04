07437defcd9a51274e0dfa8ce184613a1d932e60
MrNtex <ntexyt@gmail.com>
2025-10-04 21:43:14 +0200

    feat: Added a rent destribution chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
 import UserForm from "@/components/UserForm";
+import RentDistributionChart from "@/components/RentDistributionChart";
 
 export default function Home() {
+  const userRent = 1500;
+
   return (
-      <UserForm />
+    <div>
+      <UserForm/>
+      <h1 className="text-xl font-bold mb-4">
+        Polish ZUS Rent Distribution – March 2024
+      </h1>
+      <RentDistributionChart userRent={userRent} />
+    </div>
   );
 }
