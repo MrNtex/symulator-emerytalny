c2d7f7042fcb40795c50b0f590ae050b8ea6b107
WhatAPity171 <zpola5556@gmail.com>
2025-10-05 04:09:48 +0200

    created a function that makes a json files with how much the balance changed in each year

--- calculations/interestRateCalc.tsx
+++ calculations/interestRateCalc.tsx
@@ -1,4 +1,7 @@
 //obliczenia odnosnie subKonta
+import { updateSubBalanceAndExportJson } from './updateJson';
+const filePath = './shared/data/balance.json';
+
 interface InterestRateParams {
     initialAmount: number;
     interestRate: number;
@@ -13,10 +16,14 @@ export function calculateInterestRate(params: InterestRateParams): number {
     if (annualCapitalization) {
     for (let year = yearStart; year < yearEnd; year++) {
             amount *= (1 + interestRate);
+            updateSubBalanceAndExportJson(filePath, year.toString(), amount);
         }
     } else {
         for (let quarter = 0; quarter < (yearEnd - yearStart) * 4; quarter++) {
             amount *= (1 + interestRate/4);
+            if (quarter % 4 === 0) {
+                updateSubBalanceAndExportJson(filePath, (yearStart+quarter/4).toString(), amount);
+            }
         }
     }
     return amount;
